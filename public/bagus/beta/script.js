ccc3bd514755414e8b6aa10cd263289f888e5d29
Bagus ika Putranto <bagusikaputranto@gmail.com>
2022-06-12 11:16:30 +0700

    done beta

--- public/bagus/beta/script.js
+++ public/bagus/beta/script.js
@@ -13,19 +13,24 @@ mistageTag = document.querySelector(".mistake span"),
 timeTag = document.querySelector(".time span b"),
 wpmTag = document.querySelector(".wpm span"),
 cpmTag = document.querySelector(".cpm span");
+btnTry = document.querySelector("button");
 
 let timer,
-maxTime = 60,
+maxTime = 5,
 timeLeft = maxTime,
 charIndex = mistakes = isTyping = 0;
 
 
 function randomParagraph() {
     let randIndex = Math.floor(Math.random() * paragraphs.length);
+    typingText.innerHTML = "";
+    
     paragraphs[randIndex].split("").forEach(span => {
         let spanTag = `<span>${span}</span>`;
         typingText.innerHTML += spanTag;
     });
+    typingText.querySelectorAll("span")[0].classList.add("active");
+
     document.addEventListener("keydown", () => inpField.focus());
     typingText.addEventListener("click", () => inpField.focus());
 }
@@ -33,37 +38,42 @@ function randomParagraph() {
 function initTyping() {
     const characters = typingText.querySelectorAll("span");
     let typeChar = inpField.value.split("")[charIndex];
-    if(!isTyping){
-        timer = setInterval(initTimer, 1000);
-        isTyping = true;
-    }
-    if(typeChar == null){
-        charIndex--;
-        if(characters[charIndex].classList.contains("incorrect")){
-            mistakes--;
+    if (charIndex < characters.length - 1 && timeLeft > 0) {
+        if(!isTyping){
+            timer = setInterval(initTimer, 1000);
+            isTyping = true;
         }
-        characters[charIndex].classList.remove("correct", "incorrect");
-    }else{
-        if(characters[charIndex].innerHTML === typeChar){
-            characters[charIndex].classList.add("correct");
-            //
-            console.log("correct");
+        if(typeChar == null){
+            charIndex--;
+            if(characters[charIndex].classList.contains("incorrect")){
+                mistakes--;
+            }
+            characters[charIndex].classList.remove("correct", "incorrect");
         }else{
-            mistakes++;
-            characters[charIndex].classList.add("incorrect");
-            console.log("incorrect");
+            if(characters[charIndex].innerHTML === typeChar){
+                characters[charIndex].classList.add("correct");
+                //
+                console.log("correct");
+            }else{
+                mistakes++;
+                characters[charIndex].classList.add("incorrect");
+                console.log("incorrect");
+            }
+            charIndex++;
         }
-        charIndex++;
-    }
-    characters.forEach(span => span.classList.remove("active"));
-    characters[charIndex].classList.add("active");
-
-    let wpm = Math.round((((charIndex - mistakes) / 5) / (maxTime - timeLeft)) * 60);
+        characters.forEach(span => span.classList.remove("active"));
+        characters[charIndex].classList.add("active");
     
-    wpm = wpm < 0 || wpm === Infinity ? 0 : wpm;
-    mistageTag.innerHTML = mistakes;
-    wpmTag.innerText = wpm;
-    cpmTag.innerText = charIndex - mistakes;
+        let wpm = Math.round((((charIndex - mistakes) / 5) / (maxTime - timeLeft)) * 60);
+        
+        wpm = wpm < 0 || wpm === Infinity ? 0 : wpm;
+        mistageTag.innerHTML = mistakes;
+        wpmTag.innerText = wpm;
+        cpmTag.innerText = charIndex - mistakes;
+    } else {
+        inpField.value = "";
+        clearInterval(timer);
+    }
 }
 
 function initTimer(){
@@ -75,5 +85,19 @@ function initTimer(){
     }
 }
 
+function reset(){
+    randomParagraph();
+    inpField.value = "";
+    clearInterval(timer);
+    timeLeft = maxTime,
+    charIndex = mistakes = isTyping = 0;
+    timeTag.innerText = timeLeft;
+    mistageTag.innerText = mistakes;
+    wpmTag.innerText = 0;
+    cpmTag.innerText = 0;
+
+}
+
 randomParagraph();
 inpField.addEventListener("input", initTyping);
+btnTry.addEventListener("click", reset);
